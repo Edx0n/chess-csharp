c17a4ea63bd0450366d8f023be16774134701904
Edson <eddson.cabral@gmail.com>
2019-04-15 14:20:56 -0300

    Comecando a colocar pecas no tabuleiro

---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca  //criação objeto rei
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
---
+++ xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Torre : Peca  //criação objeto rei
+    {
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
